a044e901a23a7777a8b950d97570b3a8bff58c83
Osiris8 <miganosiris8@gmaaail.com>
2025-09-14 22:39:57 +0200

    update main file

--- main.py
+++ main.py
@@ -7,22 +7,19 @@ from app.routes.auth import auth_bp
 from app.routes.chat import chat_bp
 from app.routes.message import message_bp
 from app.routes.upload import upload_bp
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
 
-    CORS(app, supports_credentials=True)
-    db.init_app(app)
-    jwt = JWTManager(app)
-    app.register_blueprint(auth_bp, url_prefix='/api/auth')
-    app.register_blueprint(chat_bp, url_prefix='/api')
-    app.register_blueprint(message_bp, url_prefix='/api')
-    app.register_blueprint(upload_bp, url_prefix='/api')
-    with app.app_context():
+app = Flask(__name__)
+app.config.from_object(Config)
+
+CORS(app, supports_credentials=True)
+db.init_app(app)
+jwt = JWTManager(app)
+app.register_blueprint(auth_bp, url_prefix='/api/auth')
+app.register_blueprint(chat_bp, url_prefix='/api')
+app.register_blueprint(message_bp, url_prefix='/api')
+app.register_blueprint(upload_bp, url_prefix='/api')
+with app.app_context():
         db.create_all()
 
-    return app
-
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+        app.run(debug=True)
