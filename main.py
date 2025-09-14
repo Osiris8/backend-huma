308a96c337e0e5ff7f5946445ad610222d8fec91
Osiris8 <miganosiris8@gmaaail.com>
2025-09-14 23:31:40 +0200

    update main

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ from app.routes.upload import upload_bp
 app = Flask(__name__)
 app.config.from_object(Config)
 
-CORS(
-    app,
-    resources={r"/api/*": {"origins": "https://learna-zeta.vercel.app"}},
-    supports_credentials=True
-)
+CORS(app, supports_credentials=True, resources={
+    r"/api/*": {
+        "origins": [
+            "https://learna-zeta.vercel.app",
+            "http://localhost:3000"
+        ]
+    }
+})
 db.init_app(app)
 jwt = JWTManager(app)
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
