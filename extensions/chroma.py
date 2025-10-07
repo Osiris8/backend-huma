aaf73d9d3ef584b6bfd7c4e570ce06285f784a06
Osiris8 <miganosiris8@gmaaail.com>
2025-10-07 19:26:05 +0200

    replace ollama nomic embedding by atlas nomic

--- extensions/chroma.py
+++ extensions/chroma.py
@@ -1,15 +1,22 @@
+import os
 import chromadb
-import ollama
+
+from nomic import embed
+import nomic
 chroma_client = chromadb.PersistentClient(path="./chroma_db")
 
+nomic.login(os.environ.get("ATLAS_NOMIC"))
 def get_collection(chat_id: int):
     return chroma_client.get_or_create_collection(
         name=f"chat_{chat_id}",
         metadata={"dimension": 768})
 
-def embed_text(text: str):
-    response = ollama.embeddings(
-        model="nomic-embed-text",
-        prompt=text
+
+def embed_text(text: str, task_type="search_document"):
+    output = embed.text(
+        texts=[text],
+        model="nomic-embed-text-v1.5",
+        task_type=task_type,
+        dimensionality=256 
     )
-    return response["embedding"]
+    return output["embeddings"][0]
